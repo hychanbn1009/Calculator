679af0044eca8a7c84771c02b009436b5f28fbe7
Fishball Yuen <oxfishballxo@gmail.com>
2021-10-20 00:48:01 +0800

    design layout

---
+++ script.js
@@ -0,0 +1,34 @@
+const one=document.getElementById('one')
+const two=document.getElementById('one')
+const three=document.getElementById('one')
+const four=document.getElementById('one')
+const five=document.getElementById('one')
+const six=document.getElementById('one')
+const seven=document.getElementById('one')
+const eight=document.getElementById('one')
+const nine=document.getElementById('one')
+const zero=document.getElementById('one')
+const subtractButton=document.getElementById('one')
+const multiplyButton=document.getElementById('one')
+const divideButton=document.getElementById('one')
+const equalButton=document.getElementById('one')
+const clearButton=document.getElementById('one')
+
+
+const add =(number1,number2)=>{
+    return number1+number2
+}
+const subtract=(number1,number2)=>{
+    return number1-number2
+}
+const multiply=(number1,number2)=>{
+    return number1*number2
+}
+const divide=(number1,number2)=>{
+    return number1/number2
+}
+
+const operate=(operator,num1,num2)=>{
+
+}
+
