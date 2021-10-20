d4f834d1220fb772cd75f22faacd357e4edf279b
Fishball Yuen <oxfishballxo@gmail.com>
2021-10-21 00:43:58 +0800

    number & operator storage

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const one=document.getElementById('one')
-const two=document.getElementById('one')
-const three=document.getElementById('one')
-const four=document.getElementById('one')
-const five=document.getElementById('one')
-const six=document.getElementById('one')
-const seven=document.getElementById('one')
-const eight=document.getElementById('one')
-const nine=document.getElementById('one')
-const zero=document.getElementById('one')
-const subtractButton=document.getElementById('one')
-const multiplyButton=document.getElementById('one')
-const divideButton=document.getElementById('one')
-const equalButton=document.getElementById('one')
-const clearButton=document.getElementById('one')
-
+const one=document.getElementById('one').addEventListener('click',()=>{input(1)},false)
+const two=document.getElementById('two').addEventListener('click',()=>{input(2)},false)
+const three=document.getElementById('three').addEventListener('click',()=>{input(3)},false)
+const four=document.getElementById('four').addEventListener('click',()=>{input(4)},false)
+const five=document.getElementById('five').addEventListener('click',()=>{input(5)},false)
+const six=document.getElementById('six').addEventListener('click',()=>{input(6)},false)
+const seven=document.getElementById('seven').addEventListener('click',()=>{input(7)},false)
+const eight=document.getElementById('eight').addEventListener('click',()=>{input(8)},false)
+const nine=document.getElementById('nine').addEventListener('click',()=>{input(9)},false)
+const zero=document.getElementById('zero').addEventListener('click',()=>{input(0)},false)
+const addButton=document.getElementById('addButton').addEventListener('click',()=>{updateOperator('+')},false)
+const subtractButton=document.getElementById('subtractButton').addEventListener('click',()=>{updateOperator('-')},false)
+const multiplyButton=document.getElementById('multiplyButton').addEventListener('click',()=>{updateOperator('*')},false)
+const divideButton=document.getElementById('divideButton').addEventListener('click',()=>{updateOperator('/')},false)
+const equalButton=document.getElementById('equalButton')
+const clear=document.getElementById('clearButton').addEventListener('click',()=>{input('c')},false)
+const firstNumber = document.getElementById('firstNumber')
+const secondNumber = document.getElementById('secondNumber')
+const operatorContainer = document.getElementById('operatorContainer')
+const result = document.getElementById('result')
 
 const add =(number1,number2)=>{
     return number1+number2
@@ -30,5 +34,33 @@ const divide=(number1,number2)=>{
 
 const operate=(operator,num1,num2)=>{
 
+}
+
+let storeNumber1=[]
+
+const updateOperator=(operator)=>{
+    if (operator==='+'||operator==='-'||operator==='*'||operator==='/'){
+        console.log(operator)
+        operatorContainer.innerHTML=(operator)
+    }
+    else{
+        return ''
+    }
+}
+
+
+const input=(inputNumber)=>{
+    if(inputNumber==='c'){
+        storeNumber1=[]
+        firstNumber.innerHTML=resultInt(storeNumber1)
+    }else{
+        storeNumber1.push(inputNumber)
+        console.log(storeNumber1)
+        firstNumber.innerHTML=resultInt(storeNumber1)
+    }
+}
+
+const resultInt=(resultNumber)=>{
+    return resultNumber.join('')
 }
 
